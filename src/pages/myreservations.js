661a01a4a363bb7408a9a218f9d764a4b059c7ca
Vincenzo Romito <romito_vincenzo@libero.it>
2022-07-13 22:57:23 +0200

    my servations

--- src/pages/myreservations.js
+++ src/pages/myreservations.js
@@ -1,27 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-function MyReservations() {
-	const [ home, setHome ] = useState("")
+async function MyReservations() {
+  const [home, setHome] = useState("")
 
-	useEffect(() => {
-		axios.get("http://ec2-44-205-128-120.compute-1.amazonaws.com:30001/home").then(function(response) {
-			setHome(response.data)
-		})
-	}, [])
+  useEffect(() => {
+    axios.get("http://localhost:5000/myreservation").then(function (response) {
+      setHome(response.data)
+    })
+  }, [])
 
-	return (
-		<div 
-    className="App"
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '90vh'
-    }}>
-			<h1>{home}</h1>
-		</div>
-	)
+
+  return (
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}>
+      <h1>{home}</h1>
+    </div>
+  )
 }
 
 export default MyReservations;
